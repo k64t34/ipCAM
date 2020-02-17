0150db754a0bfbbc583e5fd8795300dcea408b6e
K64t34 <k64t34@gmail.com>
2020-02-18 00:03:39 +0300

    17022020

--- IPcam/Form1.Designer.cs
+++ IPcam/Form1.Designer.cs
@@ -35,6 +35,7 @@
             this.Title = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.dataSet1 = new System.Data.DataSet();
             this.button_OK = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.dataSet1)).BeginInit();
             this.SuspendLayout();
@@ -42,8 +43,9 @@
             // checkBox_AddToDesktop
             // 
             this.checkBox_AddToDesktop.AutoSize = true;
+            this.checkBox_AddToDesktop.Enabled = false;
             this.checkBox_AddToDesktop.Location = new System.Drawing.Point(392, 3);
-            this.checkBox_AddToDesktop.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.checkBox_AddToDesktop.Margin = new System.Windows.Forms.Padding(4);
             this.checkBox_AddToDesktop.Name = "checkBox_AddToDesktop";
             this.checkBox_AddToDesktop.Size = new System.Drawing.Size(219, 22);
             this.checkBox_AddToDesktop.TabIndex = 1;
@@ -53,8 +55,9 @@
             // checkBox_AutoStart
             // 
             this.checkBox_AutoStart.AutoSize = true;
+            this.checkBox_AutoStart.Enabled = false;
             this.checkBox_AutoStart.Location = new System.Drawing.Point(392, 33);
-            this.checkBox_AutoStart.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.checkBox_AutoStart.Margin = new System.Windows.Forms.Padding(4);
             this.checkBox_AutoStart.Name = "checkBox_AutoStart";
             this.checkBox_AutoStart.Size = new System.Drawing.Size(124, 22);
             this.checkBox_AutoStart.TabIndex = 2;
@@ -70,12 +73,13 @@
             this.id,
             this.Title});
             this.dataGridView1.Location = new System.Drawing.Point(2, 3);
-            this.dataGridView1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.dataGridView1.Margin = new System.Windows.Forms.Padding(4);
             this.dataGridView1.Name = "dataGridView1";
             this.dataGridView1.ReadOnly = true;
             this.dataGridView1.RowTemplate.Height = 32;
             this.dataGridView1.Size = new System.Drawing.Size(382, 338);
             this.dataGridView1.TabIndex = 3;
+            this.dataGridView1.RowEnter += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_RowEnter);
             // 
             // id
             // 
@@ -98,13 +102,23 @@
             // button_OK
             // 
             this.button_OK.Location = new System.Drawing.Point(222, 390);
-            this.button_OK.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.button_OK.Margin = new System.Windows.Forms.Padding(4);
             this.button_OK.Name = "button_OK";
             this.button_OK.Size = new System.Drawing.Size(162, 28);
             this.button_OK.TabIndex = 4;
             this.button_OK.Text = "OK";
             this.button_OK.UseVisualStyleBackColor = true;
             this.button_OK.Click += new System.EventHandler(this.button_OK_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(391, 215);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(145, 126);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "sdfggfds sgdsfgdsg\r\nsgsdfgsgf\r\nfdgsggfdsfg\r\nsgfsgsdgf\r\nsdfgsdgffdsg\r\nsfdgsdfgdsfg" +
+    "\r\nsgfdsdffdg";
             // 
             // Form1
             // 
@@ -113,12 +127,13 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(624, 442);
             this.ControlBox = false;
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.button_OK);
             this.Controls.Add(this.dataGridView1);
             this.Controls.Add(this.checkBox_AutoStart);
             this.Controls.Add(this.checkBox_AddToDesktop);
             this.Font = new System.Drawing.Font("Rod", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Margin = new System.Windows.Forms.Padding(4);
             this.MaximizeBox = false;
             this.MinimizeBox = false;
             this.Name = "Form1";
@@ -142,6 +157,7 @@
         private System.Windows.Forms.DataGridViewTextBoxColumn id;
         private System.Windows.Forms.DataGridViewTextBoxColumn Title;
         private System.Windows.Forms.Button button_OK;
+        private System.Windows.Forms.Label label1;
     }
 }
 
--- IPcam/Form1.cs
+++ IPcam/Form1.cs
@@ -15,18 +15,9 @@ namespace IPcam
     
     public partial class Form1 : Form
     {
-        //https://docs.microsoft.com/ru-ru/dotnet/framework/data/adonet/filtering-with-dataview-linq-to-dataset
-        string fCSVcam = @"\\fs1-oduyu\СПАК\Эксплуатация\IPcam\cam.csv";
-        DataSet dsCam = new DataSet("Cam");
-        DataTable tCam;// = new DataTable("Cam");
-        SqlDataAdapter  sqlCam = new SqlDataAdapter();
-        DataView  vCam;
-        //public System.Data.OleDb.OleDbConnection Conn;
-        public int selectCnt = 0;
-        public String FileDB = "cam.csv";
-        public String ProviderDB = "System.Data.Odbc";
-        public String PathDB;
-
+        public string PathDB = Path.GetDirectoryName(Application.ExecutablePath);
+        public System.Data.OleDb.OleDbConnection Conn = new System.Data.OleDb.OleDbConnection();
+        public OleDbDataAdapter adapter;
         public Form1()
         {
             InitializeComponent();
@@ -39,51 +30,47 @@ namespace IPcam
 
         private void Form1_Load(object sender, EventArgs e)
         {
+            //PathDB = @"\\fs1-oduyu\СПАК\Эксплуатация\IPcam";
+            //PathDB = @"D:\Users\Andrew\Documents\Projects\ipCAM\IPcam";
+            //PathDB = @"\\t90\tmp";
+#if DEBUG
+          
+            PathDB = System.IO.Directory.GetParent(PathDB).ToString();
+            PathDB = System.IO.Directory.GetParent(PathDB).ToString();
+            PathDB = System.IO.Directory.GetParent(PathDB).ToString();
+            PathDB += @"\DB";
+#endif
             
-            PathDB = @"\\fs1-oduyu\СПАК\Эксплуатация\IPcam";
-            PathDB = @"D:\Users\Andrew\Documents\Projects\ipCAM\IPcam";
-            PathDB = @"\\t90\tmp";
-            string PathTMP= System.IO.Path.GetTempPath().ToString();
-            PathTMP = PathTMP.Substring(0,PathTMP.Length - 1);
-            /*#if DEBUG
-                        PathDB = @"\\fs1 -oduyu\СПАК\Эксплуатация\IPcam";
-                        PathDB = "c:\\";
-            #else
-                        PathDB = Path.GetDirectoryName(Application.ExecutablePath);                  
-            #endif*/
-
-            System.Data.OleDb.OleDbConnection Conn = new System.Data.OleDb.OleDbConnection();
             Conn.ConnectionString = @"Provider = Microsoft.Jet.OLEDB.4.0;" +
-                @"Data source = "+ PathDB + ";" + //@"Data source = \\fs1-oduyu\СПАК\Эксплуатация\IPcam;"+
+                @"Data source = "+ PathDB + ";" + 
                 @"Extended Properties = ""text;HDR=YES;FMT=Delimited"";";
             try
             {
                 Conn.Open();                
                 String strSql;
                 strSql = @"select id,title from cam.csv";                
-                OleDbDataAdapter adapter;
+                
                 adapter = new OleDbDataAdapter(strSql, Conn);
                 adapter.Fill(dataSet1);
                 int iTitle = dataSet1.Tables[0].Columns.IndexOf("title");
+                //Conver codepage UTF8 to ANSI
                 foreach (DataRow row in dataSet1.Tables[0].Rows)
                     {
                     var bytes = Encoding.Convert(Encoding.Unicode, Encoding.GetEncoding(1251), Encoding.Unicode.GetBytes(row[iTitle].ToString()));
                     row[iTitle] = Encoding.UTF8.GetString(bytes);
                 }
 
-                    dataGridView1.AutoGenerateColumns = false;
+                dataGridView1.AutoGenerateColumns = false;
                 dataGridView1.BackgroundColor = this.BackColor;
-                    dataGridView1.RowHeadersVisible = false;
+                dataGridView1.RowHeadersVisible = false;
                 dataGridView1.ColumnHeadersVisible = false;
                 dataGridView1.RowHeadersWidthSizeMode =DataGridViewRowHeadersWidthSizeMode.DisableResizing;
                 dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.DisableResizing;
                 dataGridView1.AllowUserToResizeRows = false;
                 dataGridView1.BorderStyle = BorderStyle.None;
-                dataGridView1.DataSource = dataSet1.Tables[0];
-                    dataGridView1.Columns[1].DataPropertyName = "title";
-
-
-             }
+                dataGridView1.DataSource = dataSet1.Tables[0];                
+                dataGridView1.Columns[1].DataPropertyName = "title";
+            }
 		        catch (Exception ex)
 		    {
 		        MessageBox.Show("Ошибка подключения к базе данных\n"+ex.Message+
@@ -91,43 +78,12 @@ namespace IPcam
 
                     , "Установка ПО",MessageBoxButtons.OK,MessageBoxIcon.Error);		        
 		        Close();
-    }
+            }
 		    finally
 		    {
 		        Conn.Close();
 		    }
-            
-            return;
-
-            dsCam.Tables.Add("Cam");
-            tCam = dsCam.Tables["Cam"];
-            using (StreamReader sr = new StreamReader(fCSVcam, Encoding.Default))
-            {
-                // Create table header
-                string[] headers = sr.ReadLine().Split(';');
-                foreach (string header in headers)
-                {
-                    tCam.Columns.Add(header);
-                }
-                while (!sr.EndOfStream)
-                {
-                    string[] rows = sr.ReadLine().Split(';');
-                    DataRow dr = tCam.NewRow();
-                    for (int i = 0; i < headers.Length; i++)
-                    {
-                        dr[i] = rows[i];
-                    }
-                    tCam.Rows.Add(dr);                    
-                    //var query = from tCam in tCam.AsEnumerable()  select title;
-                    //EnumerableRowCollection<DataRow> query = from Cam in tCam select title;
-                    //EnumerableRowCollection<DataRow> query = from Cam in tCam select title;
-                    //vCam = query.AsDataView(); 
-                    
-                    //dataGridView1.DataSource = vCam;
-
-
-                }
-            }
+                                 
         }
 
         private void bindingSource1_CurrentChanged_1(object sender, EventArgs e)
@@ -142,7 +98,14 @@ namespace IPcam
 
         private void button_OK_Click(object sender, EventArgs e)
         {
+            //TODO: RUN VLC
             Close();
+        }
+
+        private void dataGridView1_RowEnter(object sender, DataGridViewCellEventArgs e)
+        {
+            //label1.Text = dataSet1.Tables[0].Rows[dataSet1.Tables[0].Columns.IndexOf("title")].ToString();
+            label1.Text = "as";// Convert.ToString(dataGridView1.CurrentRow.Cells[0].Size);
         }
     }
 }
